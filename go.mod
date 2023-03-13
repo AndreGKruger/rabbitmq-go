e7d12e763062882f0d58c07db7c33ab887edfd75
AndreGKruger <andre@hyvemobile.co.za>
2023-03-13 09:35:57 +0200

    intial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/AndreGKruger/rabbitmq-go
+
+go 1.19
+
+require (
+	github.com/rabbitmq/amqp091-go v1.7.0
+	go.uber.org/ratelimit v0.2.0
+)
+
+require github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
---
+++ rabbitmq-go.go
@@ -0,0 +1,201 @@
+package rabbitmqgo
+
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"go.uber.org/ratelimit"
+)
+
+type RabbitMQ interface {
+	Publish(message []byte) error
+	Consume(name string, prefetch int, handler func([]byte) error) error
+	GetStats() (amqp.Queue, error)
+	initChannel() error
+	debuglog(message string, args ...interface{})
+}
+
+type rabbitmq struct {
+	ctx            context.Context
+	logger         *log.Logger
+	debug          bool
+	durable        bool
+	connection_url string
+	config         amqp.Config
+	connection     *amqp.Connection
+	channel        *amqp.Channel
+	queue_name     string
+	producer_tps   ratelimit.Limiter
+	consumer_tps   ratelimit.Limiter
+}
+
+type RabbitMQConfig struct {
+	ctx            context.Context
+	debug          bool
+	durable        bool
+	connection_url string
+	queue_name     string
+	producer_tps   int
+	consumer_tps   int
+	config         amqp.Config
+}
+
+func NewRabbitMq(cnf RabbitMQConfig) RabbitMQ {
+	return &rabbitmq{
+		ctx:            cnf.ctx,
+		logger:         log.Default(),
+		debug:          cnf.debug,
+		durable:        cnf.durable,
+		connection_url: cnf.connection_url,
+		config:         cnf.config,
+		queue_name:     cnf.queue_name,
+		producer_tps:   ratelimit.New(cnf.producer_tps),
+		consumer_tps:   ratelimit.New(cnf.consumer_tps),
+	}
+}
+
+func (r *rabbitmq) Publish(message []byte) error {
+	// Check if the channel is initialized
+	if r.channel == nil {
+		r.debuglog("Channel is not initialized, initializing now")
+		err := r.initChannel()
+		if err != nil {
+			return err
+		}
+	}
+
+	prev := time.Now()
+	now := r.producer_tps.Take()
+	err := r.channel.PublishWithContext(
+		r.ctx,
+		"",
+		r.queue_name,
+		false,
+		false,
+		amqp.Publishing{
+			Headers:         amqp.Table{},
+			ContentType:     "text/json",
+			ContentEncoding: "",
+			Body:            message,
+			DeliveryMode:    amqp.Persistent,
+			Priority:        0,
+		},
+	)
+	now.Sub(prev)
+	prev = now
+	r.debuglog("Message published to RabbitMQ")
+	return err
+}
+
+func (r *rabbitmq) Consume(name string, prefetch int, handler func([]byte) error) error {
+	// Check if the channel is initialized
+	if r.channel == nil {
+		r.debuglog("Channel is not initialized, initializing now")
+		err := r.initChannel()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Set Channel Qos prefetch count
+	err := r.channel.Qos(prefetch, 0, false)
+	if err != nil {
+		r.debuglog("[RabbitMq] - Error setting channel Qos")
+		return err
+	}
+
+	// Consume messages
+	r.debuglog("[RabbitMq] - Consuming messages")
+	done := make(chan error)
+	go func() {
+		msgs, err := r.channel.Consume(r.queue_name, name, false, false, false, false, nil)
+		if err != nil {
+			r.debuglog("[RabbitMq] - Error consuming messages: %s", err.Error())
+			done <- err
+		}
+
+		prev := time.Now()
+		for d := range msgs {
+			now := r.consumer_tps.Take()
+			err := handler(d.Body)
+			if err != nil {
+				r.debuglog("[RabbitMq] - Error handling message: %s", err.Error())
+				r.debuglog("[RabbitMq] - Rejecting message")
+				d.Reject(false)
+				now.Sub(prev)
+				prev = now
+				done <- err
+			}
+			d.Ack(false)
+			now.Sub(prev)
+			prev = now
+		}
+	}()
+	return <-done
+}
+
+func (r *rabbitmq) GetStats() (amqp.Queue, error) {
+	// Check if the channel is initialized
+	if r.channel == nil {
+		r.debuglog("Channel is not initialized, initializing now")
+		err := r.initChannel()
+		if err != nil {
+			return amqp.Queue{}, err
+		}
+	}
+
+	queue, err := r.channel.QueueInspect(r.queue_name)
+	if err != nil {
+		r.debuglog("Error getting queue info: %s", err.Error())
+		return amqp.Queue{}, err
+	}
+
+	return queue, nil
+}
+
+// PRIVATE METHODS ---------------------------------------------------------------------------------------------
+func (r *rabbitmq) initChannel() error {
+	var err error
+
+	// Open a new connection if it hasn't been opened yet
+	r.debuglog("Opening a new connection to RabbitMQ")
+	if r.connection == nil || r.connection.IsClosed() {
+		r.connection, err = amqp.DialConfig(r.connection_url, r.config)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Open a new channel
+	r.debuglog("Opening a new channel to RabbitMQ")
+	r.channel, err = r.connection.Channel()
+	if err != nil {
+		return err
+	}
+
+	// Declare the queue
+	_, err = r.channel.QueueDeclare(
+		r.queue_name,
+		r.durable, // durable
+		false,     // autoDelete
+		false,     // exclusive
+		false,     // noWait
+		nil,       // arguments
+	)
+	if err != nil {
+		r.debuglog("Error declaring queue: %s", err.Error())
+		return err
+	}
+	r.debuglog("Queue declared")
+	return nil
+}
+
+// private function to write logs when debug is true taking in message and args
+func (r *rabbitmq) debuglog(message string, args ...interface{}) {
+	if !r.debug {
+		return
+	}
+	r.logger.Print(message, args)
+}
